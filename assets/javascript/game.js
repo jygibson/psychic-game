7b1f9cee053c4b441cf8662d11209f1c4ae1feb6
jygibson <jygibson922@gmail.com>
2018-08-28 18:00:10 -0400

    game worksgit add . adding CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@
 var wins=0;
 var loss=0;
 var guesses=9;
+var guessChoices=[];
 
 //declare the choices for the game (computer) with an array
 
@@ -12,7 +13,6 @@ var choiceArray=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p'
 
 //we need to reference the elements in html for wins, losses, guesses remaining, and probably letters already guessed
 var userGuessElement=document.getElementById("user-guess");
-var compChoiceElement=document.getElementById("zoltar-choice");
 var winsScoreElement=document.getElementById("winScore");
 var lossesScoreElement=document.getElementById("lossScore");
 var guessesRemainingElement=document.getElementById("guessRemain");
@@ -27,24 +27,32 @@ function keyPressEvent(event) {
     //update text content of elements for the guess/choice with textContent
 
 userGuessElement.textContent=userGuess;
-compChoiceElement.textContent=computerChoice;
-guessedElement.textContent=userGuess;
+guessedElement.textContent=guessChoices;
 winsScoreElement.textContent=wins;
 lossesScoreElement.textContent=loss;
 guessesRemainingElement.textContent=guesses;
 
-//create a for loop to declare what to do with each result
+//create a conditional to declare what to do with each result
 
-if (userGuess === computerChoice)
+
+if (userGuess === computerChoice){
 wins++;
-
-else if (userGuess !== computerChoice)
-losses++ ;
-guesses-- ;
+guessChoices=[];
+alert("Zoltar Speaks: You guessed it - your wish is granted!");
 }
 
+else if (userGuess != computerChoice){
+guesses-- ;
+guessChoices.push(userGuess);
+}
+
+if (guesses === 0){
+    guesses=9;
+    loss++;
+    guessChoices=[];
+}
+}
 //link it to key event
 
 document.onkeyup = keyPressEvent;
-
 
